bbef3fb3646cc3c0433dad4348ea1b991514013f
Erik Godoy <erik.sgodoy@gmail.com>
2019-09-26 12:51:29 -0300

    Ajustes no endpoint / categorias/{id}

--- src/main/java/br/com/erikgodoycursomc/demo/domain/Categoria.java
+++ src/main/java/br/com/erikgodoycursomc/demo/domain/Categoria.java
@@ -10,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Categoria implements Serializable {
   
@@ -21,6 +23,7 @@ public class Categoria implements Serializable {
   private Integer id;
   private String nome;
   
+  @JsonManagedReference
   @ManyToMany(mappedBy = "categoria")
   private List<Produto> produto = new ArrayList<>( );
   
